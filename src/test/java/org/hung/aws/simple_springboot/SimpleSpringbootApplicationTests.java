f5d70e7a696f60222a0f83665b16ec103c4d58bf
Hung Yip <kwonghung.yip@gmail.com>
2024-09-02 19:01:17 +0100

    init project

---
+++ src/test/java/org/hung/aws/simple_springboot/SimpleSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hung.aws.simple_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
