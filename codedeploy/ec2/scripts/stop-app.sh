d32cec81f32589e7b765e6633ece5f91e6b35bbb
Hung Yip <kwonghung.yip@gmail.com>
2024-09-10 12:28:06 +0100

    update stop-app.sh

--- codedeploy/ec2/scripts/stop-app.sh
+++ codedeploy/ec2/scripts/stop-app.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 echo "stop application..."
+
+DEPLOY_DIR="/opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive"
+SPRINGBOOT_PID=`cat /application/app.pid`
+
+echo "kill java process $SPRINGBOOT_PID ..."
+
+kill -9 $SPRINGBOOT_PID
