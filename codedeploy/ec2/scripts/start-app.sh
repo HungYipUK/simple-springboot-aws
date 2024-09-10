6ad6a8d9c39cde62e78b9c8382ec29e665d93bb5
Hung Yip <kwonghung.yip@gmail.com>
2024-09-10 12:04:21 +0100

    add timeout and update start-app.sh

--- codedeploy/ec2/scripts/start-app.sh
+++ codedeploy/ec2/scripts/start-app.sh
@@ -3,4 +3,4 @@ echo "start application..."
 
 DEPLOY_DIR="/opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive"
 
-nohup java -jar $DEPLOY_DIR/target/simple-springboot-0.0.1-SNAPSHOT.jar > $DEPLOY_DIR/app.log 2 >&1 & echo $! > /application/app.pid
+nohup java -jar $DEPLOY_DIR/target/simple-springboot-0.0.1-SNAPSHOT.jar > $DEPLOY_DIR/app.log 2>&1 & echo $! > /application/app.pid
