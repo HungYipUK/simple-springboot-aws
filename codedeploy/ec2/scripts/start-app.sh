335973ccefd7322243bbdc8973d8069ab6981065
Hung Yip <kwonghung.yip@gmail.com>
2024-09-10 11:27:46 +0100

    update start-app.sh

--- codedeploy/ec2/scripts/start-app.sh
+++ codedeploy/ec2/scripts/start-app.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 echo "start application..."
+
+DEPLOY_DIR="/opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive"
+
+nohup java -jar $DEPLOY_DIR/target/simple-springboot-0.0.1-SNAPSHOT.jar > $DEPLOY_DIR/app.log 2 >&1 & echo $! > /application/app.pid
