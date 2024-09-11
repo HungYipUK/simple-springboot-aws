e7ab31923157ca4c61e5b63599801c582e7bac1f
Hung Yip <kwonghung.yip@gmail.com>
2024-09-12 00:14:05 +0100

    echo with localhost hostname

--- src/main/java/org/hung/aws/EchoController.java
+++ src/main/java/org/hung/aws/EchoController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.net.Inet4Address;
+import java.net.UnknownHostException;
+
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -15,6 +18,12 @@ public class EchoController {
     @GetMapping("/echo")
     public String echo(@RequestParam(defaultValue = "john") String name) {
         log.info("request to echo endpoint " + name);
-        return "Hello " + name + "!";
+        String localhost = "";
+        try {
+            localhost = Inet4Address.getLocalHost().getHostName();
+        } catch (UnknownHostException e) {
+            log.error("Failed to get localhost hostname", e);
+        }
+        return "Hello " + name + " from " + localhost + "!";
     }
 }
